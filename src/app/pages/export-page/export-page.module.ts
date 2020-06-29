93049f71bcbf454fc519af69e9b4aa993ebfbcfa
pooja233 <66385585+pooja233@users.noreply.github.com>
2020-06-29 14:02:33 +0530

    export

--- src/app/pages/export-page/export-page.module.ts
+++ src/app/pages/export-page/export-page.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgxCsvParserModule } from 'ngx-csv-parser';
 
 import { ExportPageRoutingModule } from './export-page-routing.module';
 import { ExportPageComponent } from './export-page.component';
 
 @NgModule({
     declarations: [ExportPageComponent],
-    imports: [CommonModule, ExportPageRoutingModule],
+    imports: [CommonModule, ExportPageRoutingModule, NgxCsvParserModule],
 })
 export class ExportPageModule {}
